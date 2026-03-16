198cdecc477f5223a1a1f8e341d5694a24f90729
gyuannn <gyuannn1100@gmail.com>
2026-03-17 01:26:11 +0800

    fix: build error

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/*": [
-      "./node_modules/vscode-oniguruma/**/*",
-      "./node_modules/.pnpm/vscode-oniguruma@*/node_modules/vscode-oniguruma/**/*",
-    ],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
